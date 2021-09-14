c42b9162c1e8fc08775eaa478c9da87580033ede
VitaliyDudnik <82210074+VitaliyDudnik@users.noreply.github.com>
2021-09-14 16:44:43 +0300

    Update Constants.java

--- src/main/java/com/tms/Constants.java
+++ src/main/java/com/tms/Constants.java
@@ -1,9 +1,6 @@
 package com.tms;
 
 public interface Constants {
-    String USER_GET_BY_USERNAME = "from User where username = :username";
-    String USER_GET_BY_PASS = "from User where password = :password";
-    String CALC_GET_ALL_OPERATIONS = "from Operation";
     String REGEX_PASSWORD = "(?=.*[0-9])(?=\\S+$).{5,20}";
     String REGEX_USERNAME = "(?=\\S+$).{5,20}";
     String ATTRIBUTE_EXISTS_USERNAME_PASS = "Username or Password is incorrect";
