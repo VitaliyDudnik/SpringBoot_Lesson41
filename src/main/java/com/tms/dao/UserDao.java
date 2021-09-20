532677e8876a6c210871fc75b03592185359d445
VitaliyDudnik <82210074+VitaliyDudnik@users.noreply.github.com>
2021-09-20 09:21:06 +0300

    Update UserDao.java

--- src/main/java/com/tms/dao/UserDao.java
+++ src/main/java/com/tms/dao/UserDao.java
@@ -5,7 +5,6 @@ import com.tms.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserDao extends JpaRepository<User, Long> {
 
     boolean existsUserByUsername(String username);
