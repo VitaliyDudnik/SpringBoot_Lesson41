d6453c1d3f2551b96b1512b0e00eba426a19e3fc
VitaliyDudnik <82210074+VitaliyDudnik@users.noreply.github.com>
2021-09-20 09:20:53 +0300

    Update OperationDao.java

--- src/main/java/com/tms/dao/OperationDao.java
+++ src/main/java/com/tms/dao/OperationDao.java
@@ -4,6 +4,5 @@ import com.tms.entity.Operation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface OperationDao extends JpaRepository<Operation, Long> {
 }
